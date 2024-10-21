413835df8e86234292839c9af3bc4b0b111a931c
Happy <manncovey@gmail.com>
2024-10-20 18:23:36 -0600

    Horse wrapper

---
+++ horsewrapper.java
@@ -0,0 +1,19 @@
+/** Madison Covey
+ * Attempt two
+ * horsewrapper
+ */
+public class horsewrapper {
+
+    private  String horseName;
+    private  int year;
+
+    public static void main(String[] args) {
+        
+        Horse horse1=new Horse( horseName"Blake" , year:2009);
+        arrayList <Horse> herd= new arrayList <Horse>();
+        herd.add(Buddy);
+       System.out.println(horse1); 
+    }
+
+    
+}
